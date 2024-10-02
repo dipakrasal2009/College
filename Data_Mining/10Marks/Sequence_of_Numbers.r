378bb1cdb5d051e4c8c80448072a9036721626d0
dipakrasal2009 <dipakrasal2009@gmail.com>
2024-10-02 18:25:29 +0530

    Write a R program to create a sequence of numbers from 20 to 50 and find the mean of numbers from 20 to 60 and sum of numbers from 51 to 91.

---
+++ Data_Mining/10Marks/Sequence_of_Numbers.r
@@ -0,0 +1,13 @@
+# Creating a sequence of numbers from 20 to 50
+sequence_20_to_50 <- 20:50
+cat("Sequence from 20 to 50: \n")
+print(sequence_20_to_50)
+
+# Finding the mean of numbers from 20 to 60
+mean_20_to_60 <- mean(20:60)
+cat("Mean of numbers from 20 to 60: ", mean_20_to_60, "\n")
+
+# Finding the sum of numbers from 51 to 91
+sum_51_to_91 <- sum(51:91)
+cat("Sum of numbers from 51 to 91: ", sum_51_to_91, "\n")
+
