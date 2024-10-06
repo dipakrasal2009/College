3fcdc42ad5ef8cc1655805662dbc40237efde091
dipakrasal2009 <dipakrasal2009@gmail.com>
2024-10-06 18:43:50 +0530

    Q1. Write a program to accept a number from user. Check whether number is prime or not.

---
+++ java/Slip/10Marks/ten.java
@@ -0,0 +1,38 @@
+//Q1. Write a program to accept a number from user. Check whether number is prime or not.
+//:-
+//
+
+import java.util.Scanner;
+
+//prime check
+public class ten {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
+
+    // Function to check if a number is prime
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
